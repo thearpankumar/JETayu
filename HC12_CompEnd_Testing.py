1e98159196c4ea76c84218d751d7ab3454d91149
thearpankumar <arpankumar1119@outlook.com>
2023-04-19 23:36:57 +0530

    HC12

--- HC12_CompEnd_Testing.py
+++ HC12_CompEnd_Testing.py
@@ -1,18 +1,18 @@
 import serial
+"""import serial
 import struct
 
 ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
 
 while True:
-    line = ser.readline()#.decode("utf-8")
-    #print(line)
-    
+    line = ser.readline()
+
     #ser.write(b"?\n")
-    
+
     data = int.from_bytes(line[:-2], byteorder='big', signed=False)
 
 # print data
-    print(data)
+    print(data)"""
 
 """
 Sabertooth_Serial_motorA = serial.Serial(
@@ -24,3 +24,8 @@ Sabertooth_Serial_motorA = serial.Serial(
 
 Sabertooth_Serial_motorA.read(struct.pack(">B", int(self.data.replace("left"))))
 """
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)  # HC-12 serial port
+while True:
+    message = ser.readline().decode().strip()
+    print("Received message: ", message)
